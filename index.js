85eeeed67df6118bb2f0a317b05ec8e40363b359
Ashakuur1 <engshakrayare114@gmail.com>
2023-10-13 01:06:46 +0300

    fixing

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Import server here and start the application
 import server from './api/server.js';
+import cors from 'cors';
+
+
 
 const port = 4000;
 
+
+server.use(
+    cors({
+        origin : '*',
+        methods : '*',
+        credentials: true,
+    })
+)
 server.listen(port,() => {console.log(`listening ${port}`);})
+
